47461703ea666775c5ad51300846bd0edf5accee
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-04 14:41:32 +0300

    Types added to tests folder for the future usage

---
+++ packages/db-adapter/tests/types.ts
@@ -0,0 +1,11 @@
+// hardcoded types that will be used
+
+type LahjalistaUser = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  createdAt: Date;
+  updatedAt: Date;
+  uuid: string;
+  role: string;
+};
