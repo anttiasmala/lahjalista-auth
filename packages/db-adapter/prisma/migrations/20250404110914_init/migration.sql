62fd836eea0eb777c6f0eb59c82a14952a04f57c
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-09 21:06:31 +0300

    Update User to have needed rows and add enum Role to schema.prisma. Create a migration of it

--- packages/db-adapter/prisma/migrations/20250404110914_init/migration.sql
+++ packages/db-adapter/prisma/migrations/20250404110914_init/migration.sql
@@ -1,7 +1,15 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "uuid" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -22,6 +30,9 @@ CREATE TABLE "Session" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_uuid_key" ON "User"("uuid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Session_uuid_key" ON "Session"("uuid");
